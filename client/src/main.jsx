502ebf53986c496cdde3607b87a1672277ae9428
adam <adammarey51@gmail.com>
2024-04-18 15:28:29 -0700

    database tested

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,41 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { createRoot } from 'react-dom/client'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios';
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchNotes = async () => {
-      const response = await axios.get('/api/notes')
-      setNotes(response.data)
-      setIsLoading(false)
-    }
-    fetchNotes()
-  }, [])
+      const response = await axios.get('/api/notes');
+      setNotes(response.data);
+      setIsLoading(false);
+    };
+    fetchNotes();
+  }, []);
 
   if (isLoading) {
-    return <section className="loading">Loading</section>
+    return <section className="loading">Loading</section>;
   }
 
   return (
     <main>
-      <h1>Acme Notes ({notes.length})</h1>
+      <h1>
+        Acme Notes ({notes.length})
+      </h1>
       <ul>
-        {notes.map((note) => {
+        {notes.map(note => {
           return (
             <li key={note.id}>
-              {note.txt}
-              {note.starred ? '⭐' : null}
+              <h2>
+                {note.firstname} {note.lastname}
+              </h2>
+              <p>
+                {note.occupation}
+              </p>
+              <p>
+                {note.service}
+              </p>
+              <p>
+                {note.email}
+              </p>
+              <p>
+                {note.phone}
+              </p>
+              <img src={note.photo_url} alt="photo" />
             </li>
-          )
+          );
         })}
       </ul>
     </main>
-  )
-}
+  );
+};
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector('#root'));
 
-root.render(<App />)
+root.render(<App />);
--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const app = express();
 const path = require('path');
 const pg = require('pg');
 
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_db')
+const client = new pg.Client(
+  process.env.DATABASE_URL || 'postgres://localhost/arizona'
+);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/', (req, res, next)=> {
+app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
@@ -15,31 +17,36 @@ app.get('/api/notes', async (req, res, next) => {
   try {
     const SQL = `
       SELECT * from notes;
-    `
-    const response = await client.query(SQL)
-    res.send(response.rows)
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
   } catch (ex) {
-    next(ex)
+    next(ex);
   }
-})
+});
 
 const init = async () => {
-  await client.connect()
+  await client.connect();
   const SQL = `
     DROP TABLE IF EXISTS notes;
     CREATE TABLE notes(
       id SERIAL PRIMARY KEY,
-      txt VARCHAR(255),
-     starred BOOLEAN DEFAULT FALSE
+      firstname VARCHAR(255),
+      lastname VARCHAR(255),
+      occupation VARCHAR(255),
+      service TEXT,
+      website  VARCHAR(255),
+      email VARCHAR(100) UNIQUE NOT NULL,
+      phone VARCHAR(15),
+      photo_url VARCHAR(255)
     );
-    INSERT INTO notes(txt, starred) VALUES('learn express', false);
-    INSERT INTO notes(txt, starred) VALUES('write SQL queries', true);
-   INSERT INTO notes(txt) VALUES('create routes');
-  `
-  await client.query(SQL)
-console.log('data seeded')
-  const port = process.env.PORT || 3000
-  app.listen(port, () => console.log(`listening on port ${port}`))
-}
+    INSERT INTO notes (firstname, lastname, occupation, service, website, email, phone, photo_url)
+    VALUES ('John', 'Doe', 'Software Engineer', 'Google', 'www.google.com', 'john@gmail.com', '1234567890', 'https://imageio.forbes.com/specials-images/imageserve/64b5825a5b9b4d3225e9bd15/artificial-intelligence--ai/960x0.jpg?format=jpg&width=960');
+  `;
+  await client.query(SQL);
+  console.log('data seeded');
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
 
-init()
+init();
