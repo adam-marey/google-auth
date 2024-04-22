309337f360b456f0e8913a7b4447b8003967ae44
adam <adammarey51@gmail.com>
2024-04-22 12:32:30 -0700

    chnaged notes to users

--- server/index.js
+++ server/index.js
@@ -15,23 +15,29 @@ app.use('/api', router);
 const init = async () => {
   await client.connect();
   const SQL = `
-    DROP TABLE IF EXISTS notes;
-    CREATE TABLE notes(
+    DROP TABLE IF EXISTS users;
+    CREATE TABLE users(
       id SERIAL PRIMARY KEY,
       firstname VARCHAR(255),
       lastname VARCHAR(255),
       occupation VARCHAR(255),
-      service TEXT,
-      website  VARCHAR(255),
+      company TEXT,
+      website VARCHAR(255),
       email VARCHAR(100) UNIQUE NOT NULL,
       phone VARCHAR(15),
       photo_url VARCHAR(255)
     );
-    INSERT INTO notes (firstname, lastname, occupation, service, website, email, phone, photo_url)
-    VALUES ('John', 'Doe', 'Software Engineer', 'Google', 'www.google.com', 'john@gmail.com', '1234567890', 'https://imageio.forbes.com/specials-images/imageserve/64b5825a5b9b4d3225e9bd15/artificial-intelligence--ai/960x0.jpg?format=jpg&width=960');
+
+    INSERT INTO users (firstname, lastname, occupation, company, website, email, phone, photo_url)
+    VALUES 
+      ('Adam', 'Marey', 'Software Engineer', 'Google', 'www.google.com', 'john@google.com', '1234567890', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png'),
+      ('William', 'Smith', 'Data Analyst', 'Facebook', 'www.facebook.com', 'alice@facebook.com', '9876543210', 'https://1000logos.net/wp-content/uploads/2017/02/Facebook-Logosu.png'),
+      ('Andrew', 'Johnson', 'Web Developer', 'Amazon', 'www.amazon.com', 'bob@amazon.com', '5556667777', 'https://i.pinimg.com/736x/0a/06/60/0a06600cc3cedeb49280b54114c88ce6.jpg');
   `;
+
   await client.query(SQL);
   console.log('data seeded');
+
   const port = process.env.PORT || 3000;
   app.listen(port, () => console.log(`listening on port ${port}`));
 };
--- server/routes.js
+++ server/routes.js
@@ -6,10 +6,10 @@ const client = new pg.Client(
   process.env.DATABASE_URL || 'postgres://localhost/arizona'
 );
 
-router.get('/notes', async (req, res, next) => {
+router.get('/users', async (req, res, next) => {
   try {
     const SQL = `
-      SELECT * from notes;
+      SELECT * from users;
     `;
     const response = await client.query(SQL);
     res.send(response.rows);
@@ -18,9 +18,9 @@ router.get('/notes', async (req, res, next) => {
   }
 });
 
-router.get('/notes/:id', async (req, res, next) => {
+router.get('/users/:id', async (req, res, next) => {
   const { id } = req.params;
-  const SQL = `SELECT * FROM notes WHERE id=$1;`;
+  const SQL = `SELECT * FROM users WHERE id=$1;`;
   const values = [id];
   try {
     const data = await client.query(SQL, values);
@@ -34,19 +34,19 @@ router.get('/notes/:id', async (req, res, next) => {
   }
 });
 
-router.post('/notes', async (req, res, next) => {
+router.post('/users', async (req, res, next) => {
   const {
     firstname,
     lastname,
     occupation,
-    service,
+    company,
     website,
     email,
     phone,
     photo_url
   } = req.body;
   const SQL = `
-    INSERT INTO notes (firstname, lastname, occupation, service, website, email, phone, photo_url)
+    INSERT INTO users (firstname, lastname, occupation, company, website, email, phone, photo_url)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     RETURNING *;
   `;
@@ -54,7 +54,7 @@ router.post('/notes', async (req, res, next) => {
     firstname,
     lastname,
     occupation,
-    service,
+    company,
     website,
     email,
     phone,
@@ -72,9 +72,9 @@ router.post('/notes', async (req, res, next) => {
   }
 });
 
-router.delete('/notes/:id', async (req, res, next) => {
+router.delete('/users/:id', async (req, res, next) => {
   const { id } = req.params;
-  const SQL = `DELETE FROM notes WHERE id=$1;`;
+  const SQL = `DELETE FROM users WHERE id=$1;`;
   const values = [id];
   try {
     await client.query(SQL, values);
@@ -85,21 +85,21 @@ router.delete('/notes/:id', async (req, res, next) => {
   }
 });
 
-router.put('/notes/:id', async (req, res, next) => {
+router.put('/users/:id', async (req, res, next) => {
   const { id } = req.params;
   const {
     firstname,
     lastname,
     occupation,
-    service,
+    company,
     website,
     email,
     phone,
     photo_url
   } = req.body;
   const SQL = `
-    UPDATE notes 
-    SET firstname=$1, lastname=$2, occupation=$3, service=$4, website=$5, email=$6, phone=$7, photo_url=$8
+    UPDATE users 
+    SET firstname=$1, lastname=$2, occupation=$3, company=$4, website=$5, email=$6, phone=$7, photo_url=$8
     WHERE id=$9
     RETURNING *;
   `;
@@ -107,7 +107,7 @@ router.put('/notes/:id', async (req, res, next) => {
     firstname,
     lastname,
     occupation,
-    service,
+    company,
     website,
     email,
     phone,
